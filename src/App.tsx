870e830aadc1e5dfaf20f062a46d315f257c0fa7
duhanmeric <duhanmeric@gmail.com>
2023-03-31 21:12:00 +0300

    redux relocated to index

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import "./index.css";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
 import { Header } from "@/components";
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="app">
-        <Header />
-      </div>
-    </Provider>
+    <div className="app">
+      <Header />
+    </div>
   );
 }
 
